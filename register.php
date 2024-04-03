d56c6ebc9ba505065628ddf11e61126e99e9ea31
Anaïs Kajjaj <100074659+Anaderis@users.noreply.github.com>
2024-04-03 11:37:31 +0200

    add

--- register.php
+++ register.php
@@ -1,171 +1,156 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/iconelogin" href="./Assets/icon-Login.png" />
     <link rel="stylesheet" href="./css/login.css">
     <title>Sign In</title>
 
-    <!-- Footer -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css"> 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-    
-    <!-- Police -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Anta&display=swap" rel="stylesheet">
-
-    <!-- Include jQuery -->
-    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-
-
 </head>
+
 <body>
-    <header>
-        <nav>
-            <a href="./bdd-mount.php">
-                <img class="logoWOW" src="./Assets/logo.png" alt="logoWOW">
-            </a>
-        </nav>
-    </header>
-    <main>
-        <!-- Login -->
-        <div class="loginConteneur">
-            <div class="loginhtml">
-                <div class="loginbutton ombre backgroundBlanc itemcenter">
-                    <input id="tab-signin" type="radio" name="tab" class="login"  onclick="hide_register()">
-                    <label for="tab-signin" class="tab">Sign In</label>
-                </div>
-                <div class="registerbutton ombre backgroundBlanc itemcenter">
-                    <input id="tab-signup" type="radio" name="tab" class="register" checked onclick="hide_login()">
-                    <label for="tab-signup" class="tab">Sign Up</label>
-                </div>
-                <div class="loginform " >
-                    <div class="loginbox ombre backgroundBlanc" id="login_vue">
-                        <div class="titre-login">
-                            <h2>Sign In</h2>
-                        </div>
-                        <form action="login.php" method="POST">
-                            <div class="loginholder">
-                                <div class="divloginholder">
-                                    <h6>UserName</h6> 
-                                    <input type="text" name="User" placeholder="Enter your UserName">
-                                </div>
-                                <div class="divloginholder">
-                                    <h6>PassWord</h6>
-                                    <input type="password" name="password" placeholder="Enter your PassWord">
-                                </div>
-                                <div class="checkin">
-                                    <input id="check" type="checkbox" class="check">
-                                    <label for="check"><span class="icon"></span> Keep me Signed in</label>
-                                </div>
-                                <div class="submitbuttonlogin">
-                                    <input type="submit" name="submit"  value="Login">
-                                </div>
-                                <div class="hrloginconteneur">
-                                    <div class="footerboxlogin">
-                                        <label class="forgotpassword" for="tab-signup">Forgot Password?</a>
-                                    </div>
-                                </div>
-                                
+
+<section class="vh-100" style="background-color: #2779e2;">
+  <div class="container h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-xl-9">
+        <h1 class="text-white mb-4">Sign up</h1>
+            <form action="register.php" method="post" enctype="multipart/form-data">
+                <div class="card" style="border-radius: 15px;">
+                    <div class="card-body">
+
+                        <div class="row align-items-center pt-4 pb-3">
+                            <div class="col-md-3 ps-5">
+
+                                <h6 class="mb-0">Email Adress</h6>
+
                             </div>
-                        </form> 
-                    </div>
-                    
-                    <div class="registerbox ombre backgroundBlanc" id="register_vue">
-                        <div class="titre-login">
-                            <h2>Sign Up</h2>
+                            <div class="col-md-9 pe-5">
+                            
+                                <input type="email" class="form-control form-control-lg" id="email-register" name="email-register" placeholder="Enter your Email Adress">
+                                        
+                            </div>
                         </div>
-                        
-                        <form action="register.php" method="post" enctype="multipart/form-data">
-                            <div class="registerholder">
-                                <div class="textholder">
-                                    <div class="textregisterholder">
-                                        <div class="divloginholder">
-                                            <h6>Email Adress</h6>
-                                            <input type="email" id="email-register" name="email-register" placeholder="Enter your Email Adress">
-                                        </div>
-                                        <div class="divloginholder">
-                                            <h6>UserName</h6>
-                                            <input type="text" name="User-register" placeholder="Enter your UserName">
-                                        </div>
-                                    </div>
-                                    <div class="pwdregisterholder">
-                                        <div class="divloginholder">
-                                            <h6>PassWord</h6>
-                                            <input type="password" name="password-register" placeholder="Enter your PassWord">
-                                        </div>
-                                        <div class="divloginholder">
-                                            <h6>Repeat PassWord</h6> 
-                                            <input type="password" name="password-check-register" placeholder="Repeat your PassWord">
-                                        </div>
-                                        <div class="divloginholder">
-                                            <label for="type">You are :</label>
-                                                <select name="type">
+
+
+                        <hr class="mx-n3">
+
+                        <div class="row align-items-center py-3">
+                            <div class="col-md-3 ps-5">
+
+                                <h6 class="mb-0">Username</h6>
+ 
+                            </div>
+                            <div class="col-md-9 pe-5">
+
+                            <input type="text" name="User-register" class="form-control" rows="3" placeholder="Enter your UserName">
+                            
+                            </div>
+                        </div>
+
+                        <hr class="mx-n3">
+
+                        <div class="row align-items-center py-3">
+                            <div class="col-md-3 ps-5">
+
+                                <h6 class="mb-0">PassWord</h6>
+ 
+                            </div>
+                            <div class="col-md-9 pe-5">
+
+                            <input type="password" name="password-register" class="form-control" rows="3" placeholder="Enter your password">
+                            
+                            </div>
+                        </div>
+
+                        <hr class="mx-n3">
+
+                        <div class="row align-items-center py-3">
+                            <div class="col-md-3 ps-5">
+
+                                <h6 class="mb-0">Repeat PassWord</h6>
+ 
+                            </div>
+                            <div class="col-md-9 pe-5">
+
+                            <input type="password" name="password-check-register" class="form-control" rows="3" placeholder="Repeat your password">
+                            
+                            </div>
+                        </div>
+
+                        <hr class="mx-n3">
+
+                        <div class="row align-items-center py-3">
+
+                            <div class="col-md-3 ps-5">
+
+                                <h6 class="mb-0"><label for="type">You are :</label></h6>
+ 
+                            </div>
+                            <div class="col-md-9 pe-5">
+
+                            <select name="type" class="form-control" rows="3">
                                                     <option value="">...</option>
                                                     <option value="admin">An administrator</option>
                                                     <option value="creator">A creator</option>
                                                     <option value="player">A player</option>
-                                                </select>
-                                        </div>
+                            </select>  
 
-                                        <label for="file">Upload your game</label>
-                                        <input type="file" name="file"> 
-                                    
-                                    </div>
+                            </div>
+                        </div>
+
+                        <hr class="mx-n3">
+
+                            <div class="row align-items-center py-3">
+                                <div class="col-md-3 ps-5">
+
+                                    <h6 class="mb-0"><label for="file">Upload your game file</label></h6>
+
                                 </div>
-                                <?php include "./PHP/Login/adduser.php"?>
-                                <div class="submitbuttonregister">
-                                    <input type="submit" name="submit-register" value="Register">
-                                </div>
-                                
-                                <div class="hrregisterconteneur">
-                                    <div class="footerboxregister">
-                                        <label class="alreadymember" for="tab-signin">Already Member?</a>
-                                    </div>
+                                <div class="col-md-9 pe-5">
+
+                                <input class="form-control form-control-lg" id="formFileLg" type="file" name="file" />
+                                <div class="small text-muted mt-2">Upload your CV/Resume or any other relevant file. Max file
+                                size 50 MB</div>
+
                                 </div>
                             </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
+
+                            <hr class="mx-n3">
+
+                                <div class="px-5 py-4">
+                                    <?php include "./PHP/Login/adduser.php"?>
+                                    <input type="submit" name="submit-register" value="Register" class="btn btn-primary btn-lg">
+                                </div>
+
+                        </div>
         </div>
+      </div>
+    </div>
+  </div>
+</section>
+                        
+
+
+
+                                        
+                    
+                                            
+                                                
+                                       
+
+                                        
+                                        
+                                
+                                
+                                    
+                                
+                                
+                                
     
-        <script>
-            //Func hide_register()
-                //Récupération element avec ID = login_vue
-                //element.attribute.set(hide = false)
-                //Récupération element avec ID = register_vue
-                //element.attribute.set(hide = true)
-            //Func hide_login()
-                //Récupération element avec ID = login_vue
-                //element.attribute.set(hide = true)
-                //Récupération element avec ID = register_vue
-                //element.attribute.set(hide = false)
-                function hide_register() {
-                    document.getElementById("register_vue").style.display = "none";
-                    document.getElementById("login_vue").style.display = "block";
-                }
 
-                function hide_login() {
-                    document.getElementById("login_vue").style.display = "none";
-                    document.getElementById("register_vue").style.display = "block";
-                }
 
-                // Simulate a click on the "Sign Up" tab when the page loads
-                    document.addEventListener("DOMContentLoaded", function() {
-                    document.getElementById("tab-signup").click();
-                });
-        </script>
-        <style>
-            #register_vue {
-                display: none;
-            }
-            .registerbox{
-                margin-bottom: 5%; 
-            }
-        </style>
-    </main> 
 </body>
 </html>
