629e3befa46f49317dd092aa52cc5729ef63753f
Anaïs Kajjaj <100074659+Anaderis@users.noreply.github.com>
2024-04-09 10:19:03 +0200

    add

--- PHP/Login/adduser.php
+++ PHP/Login/adduser.php
@@ -179,9 +179,8 @@ if(isset($_POST["submit-register"])){
         $sanitizedEmail = sanitizeEmail($emailregister);
         
         // Check si l'email existe déjà dans la base de données
-        $sqlCheck = "SELECT user_email FROM t_user WHERE user_email = ':sanitizedemail'";
+        $sqlCheck = "SELECT user_email FROM t_user WHERE user_email = '$sanitizedEmail'";
         $result = $conn->query($sqlCheck);
-        $result ->bindParam(':sanitizedemail', $sanitizedEmail, PDO::PARAM_STR);
 
         if ($result->num_rows > 0) {
             array_push($errors, "Email already exist." );
